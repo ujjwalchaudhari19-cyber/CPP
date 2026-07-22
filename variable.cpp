d4ea02a8b4335e1dcceac526c10dbd7ae17f1773
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-07-22 13:37:49 +0530

    Understanding the variables and the rules to assign the variables

---
+++ variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string name = "Tom";
+  int age;
+  age = 50;
+
+  cout<< "Ther once was a man named "<< name <<endl;
+  cout << "He was "<< age <<  " years old" << endl;
+  name = "Mike";
+  cout << "He liked the name "<< name << endl;
+  cout << "But did not like being " <<age << endl;
+
+
+  return 0;
+}
