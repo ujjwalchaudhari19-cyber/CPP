bd0eb100548e2a98f10e774d7fa4f56731b07b67
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-07-22 13:53:42 +0530

    What are different types of data types in C++

---
+++ datatype.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+  char grade = 'A';
+  string phrase = "Giraffe Academy";
+  int number = 67;
+  double decimal = 67.67;
+  bool isMale = true;
+
+  cout << grade << endl;
+  cout << phrase << endl;
+  cout << number << endl;
+  cout << decimal << endl;
+  cout << isMale << endl;
+
+  return 0;
+}
