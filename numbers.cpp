1d03b95c10cf0a7d4ebd2cd0510bcf77a63d3fdb
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-07-24 22:15:21 +0530

    Working with numbers

---
+++ numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int wnum = 5;
+  double dnum = 5.5;
+  cout << 5+7 <<endl;
+  cout << 10 % 3 <<endl;
+  cout << wnum << endl;
+  cout << ++wnum << endl;
+
+
+  return 0;  
+} 
