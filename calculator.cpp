a50c67fa2db53908136840fb6ce4873705069fac
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-07-25 14:18:51 +0530

    Built a basic calculator

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+  double num1, num2;
+  cout << "Enter first number: ";
+  cin >> num1;
+
+  cout << "Enter Second number: ";
+  cin >> num2;
+
+  cout << "Addition of 2 numbers is: " << num1+num2 << endl;
+  cout << "Multiplication of 2 numbers is: " << num1*num2 << endl;
+  cout << "Division of 2 numbers is: " << num1/num2 << endl;
+  cout << "Subtraction of 2 numbers is: " << num1-num2 << endl;
+
+  return 0;
+}
