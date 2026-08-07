4b335281088b770e4cdef55d396d3d594a8029dc
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-08-07 23:59:21 +0530

    Using while and do while loops

---
+++ Basic C++/doWhileLoop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int number = 0;
+  int sum = 0;
+
+  do{
+    sum += number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+  }
+  while(number >= 0);
+  cout << "The sum is " << sum << endl;
+  
+
+  return 0;
+}
---
+++ Basic C++/whileLoops-02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int number;
+  int sum = 0;
+
+  cout << "Enter a number: ";
+  cin >> number;
+
+  while(number >= 0){
+    sum += number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+  }
+
+  cout << "The sum is " << sum << endl;
+}
